ee91ec94dad3a65c0efa805f1b493fc6d08ef690
Paul Cortez <paulcortez0591@yahoo.com>
2020-01-31 17:22:22 +0800

    read directory

---
+++ DataObj/FileAndDirectory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LiteDB;
+
+namespace YearBookViewer.DataObj
+{
+    public class FileAndDirectory
+    {
+        [BsonId]
+        public int Id { get; set; }
+        public bool isFile { get; set; }
+        public string filename { get; set; }
+        public string filepath { get; set; }
+        public bool scanned { get; set; }
+        public DateTime lastScanned { get; set; }
+    }
+}

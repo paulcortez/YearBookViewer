676119a94969590dc6b7795af41724a3057e0078
Paul Cortez <paulcortez0591@yahoo.com>
2020-02-03 10:50:06 +0800

    files

---
+++ DataObj/Document.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LiteDB;
+
+namespace YearBookViewer.DataObj
+{
+    public class Document
+    {
+        [BsonId]
+        public int Id { get; set; }
+        public string DocumentTitle { get; set; }
+        public string DocumentFolderPath { get; set; }
+        public int TotalPages { get; set; }
+        public List<DocumentPages> DocPages { get; set; }
+
+    }
+}
